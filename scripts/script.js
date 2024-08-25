9095a5f76b9802005d578c8fa96f9abc9c5dc891
Reagan PC2 <reaganfukibulu43@gmail.com>
2024-08-25 19:16:24 +0200

    modified:   index2.html 	modified:   scripts/script.js 	modified:   styles/style2.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 const button = document.querySelector(".button");
 const menu = document.querySelector(".menu");
+const lien1 = document.querySelector('#lien1');
+const lien2 = document.querySelector('#lien2');
+const lien3 = document.querySelector('#lien3');
 
 button.onclick = function () {
+    button.classList.toggle("active");
+    menu.classList.toggle("active");
+};
+
+lien1.onclick = function () {
+    button.classList.toggle("active");
+    menu.classList.toggle("active");
+};
+lien2.onclick = function () {
+    button.classList.toggle("active");
+    menu.classList.toggle("active");
+};
+lien3.onclick = function () {
     button.classList.toggle("active");
     menu.classList.toggle("active");
 };
