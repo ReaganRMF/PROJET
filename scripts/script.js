e14c1c5fc13d2cd42148d7e4440c98c5a2994b26
ReaganRMF <reaganfukibulu43@gmail.com>
2024-08-27 07:28:47 +0100

    modified:   index.html 	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,3 @@
-const button = document.querySelector(".button");
-const menu = document.querySelector(".menu");
-const lien1 = document.querySelector("#lien1");
-const lien2 = document.querySelector("#lien2");
-const lien3 = document.querySelector("#lien3");
-
-button.onclick = function () {
-    button.classList.toggle("active");
-    menu.classList.toggle("active");
-};
-
-lien1.onclick = function () {
-    button.classList.toggle("active");
-    menu.classList.toggle("active");
-};
-lien2.onclick = function () {
-    button.classList.toggle("active");
-    menu.classList.toggle("active");
-};
-lien3.onclick = function () {
-    button.classList.toggle("active");
-    menu.classList.toggle("active");
-};
-
 document.addEventListener("DOMContentLoaded", () => {
     const wineCards = document.querySelectorAll(".wine-card");
 
